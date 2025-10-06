7e6a30b1787f0080a61fd576811f58b88e2f4e69
veerannadeshanur11 <shreyasdeshanur34@gmail.com>
2025-10-06 15:31:09 +0530

    Update si.py

--- si.py
+++ si.py
@@ -7,6 +7,8 @@ time = float(input("Enter the Time (in years): "))
 
 # Calculate Simple Interest
 si = (principal * rate * time) / 100
+ci = principal * ((1 + rate / 100) ** time) - principal
 
 # Display the result
 print("Simple Interest =", si)
+print("Compound Intrest=",ci)
